727480b225518b0e37b3778f60693ad9126a6824
azizjan.ayupov <azizjan.ayupov@gmail.com>
2024-07-04 12:06:36 +0600

    lang

--- MainConsole/BotSettings.cs
+++ MainConsole/BotSettings.cs
@@ -4,6 +4,6 @@ namespace MainConsole;
 
 public static class BotSettings
 {
-    public const string Token = "7058640374:AAEQ_HDQYgeoNuFqmPtkdmNBQ7Uf4S_hMjw";
+    public const string Token = "7058640374:AAG6WVuUY6b3xEdU-fStZLwbpuA0mUc8PtU";
     
 }
--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -1,65 +1,113 @@
 ﻿using MainConsole;
+using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
-TelegramBotClient client = new (BotSettings.Token);
 
-CancellationTokenSource source = new ();
-
-// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
-ReceiverOptions receiverOptions = new()
+try
 {
-    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
-};
+    CancellationTokenSource source = new();
 
-client.StartReceiving(
-    updateHandler: HandleUpdateAsync,
-    pollingErrorHandler: HandlePollingErrorAsync,
-    receiverOptions: receiverOptions,
-    cancellationToken: source.Token
-);
+    TelegramBotClient bot = new(BotSettings.Token, cancellationToken: source.Token);
 
-var me = await client.GetMeAsync();
+    const string lang_ru = "Русский";
+    const string lang_ky = "Кыргызча";
 
-Console.WriteLine($"Start listening for @{me.Username}");
-Console.ReadLine();
+    string? lang = null;
 
-// Send cancellation request to stop bot
-source.Cancel();
-
-async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-{
-    // Only process Message updates: https://core.telegram.org/bots/api#message
-    if (update.Message is not { } message)
-        return;
-    // Only process text messages
-    if (message.Text is not { } messageText)
-        return;
-
-    var chatId = message.Chat.Id;
-
-    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
-
-    // Echo received message text
-    Message sentMessage = await botClient.SendTextMessageAsync(
-        chatId: chatId,
-        text: "You said:\n" + messageText,
-        cancellationToken: cancellationToken);
-}
-
-Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-{
-    var ErrorMessage = exception switch
+    // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+    ReceiverOptions receiverOptions = new()
     {
-        ApiRequestException apiRequestException
-            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-        _ => exception.ToString()
+        AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
     };
 
-    Console.WriteLine(ErrorMessage);
-    return Task.CompletedTask;
-}
+    async Task OnCallbackQuery(CallbackQuery callbackQuery)
+    {
+        if(callbackQuery.Data == lang_ru)
+        {
+            lang = lang_ru;
 
+            await bot.AnswerCallbackQueryAsync(callbackQuery.Id, callbackQuery.Data);
+
+            List<List<KeyboardButton>> rusKeys =
+          [
+              ["Выбор позиции", "Список вакансий"]
+          ];
+            await bot.SendTextMessageAsync(
+                callbackQuery.Message!.Chat.Id, 
+                "Русский язык установлен",
+                replyMarkup: new ReplyKeyboardMarkup(rusKeys) { ResizeKeyboard = true });
+        }
+
+        if(callbackQuery.Data == lang_ky)
+        {
+            lang = lang_ky;
+
+            await bot.AnswerCallbackQueryAsync(callbackQuery.Id, callbackQuery.Data);
+
+            List<List<KeyboardButton>> rusKeys =
+           [
+               ["Позиция тандоо", "Вакансиялардын тизмеси"]
+           ];
+            await bot.SendTextMessageAsync(callbackQuery.Message!.Chat.Id, 
+                "Кыргыз тили орнотулду",
+                 replyMarkup: new ReplyKeyboardMarkup(rusKeys) { ResizeKeyboard = true });
+        }    
+
+    }
+
+    var kyrBtn = InlineKeyboardButton.WithCallbackData(lang_ky);
+    var rusBtn = InlineKeyboardButton.WithCallbackData(lang_ru);
+
+    InlineKeyboardMarkup langKeyboard =
+        new([kyrBtn, rusBtn]);
+    
+    bot.StartReceiving(OnUpdate, async (bot, ex, ct) => Console.WriteLine(ex));
+
+    var me = await bot.GetMeAsync();
+
+    Console.WriteLine($"Start listening for @{me.Username}");
+    Console.ReadLine();
+
+    // Send cancellation request to stop bot
+    source.Cancel();
+
+    async Task OnUpdate(ITelegramBotClient bot, Update update, CancellationToken ct)
+    {
+        await (update switch
+        {
+            { Message: { } message } => OnMessage(message),
+           //{ EditedMessage: { } message } => OnMessage(message, true),
+            { CallbackQuery: { } callbackQuery } => OnCallbackQuery(callbackQuery),
+            _ => OnUnhandledUpdate(update)
+        });
+    }
+
+    async Task OnMessage(Message message)
+    {
+        var chatId = message.Chat.Id;
+        var messageText = message.Text;
+
+        Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+        if (messageText == "/start" || messageText == "/lang")
+        {
+            Message sentMessage = await bot.SendTextMessageAsync(
+               chatId: chatId,
+               text: "Выберите язык\n\nТилди тандаңыз",
+               replyMarkup: langKeyboard,
+               cancellationToken: source.Token);
+        }
+    }
+    async Task OnUnhandledUpdate(Update update) => Console.WriteLine($"Received unhandled update {update.Type}");
+    
+}
+catch (Exception ex)
+{
+
+	throw;
+}
