df12ad2b4b655b6fdc3a30e3c1ba6b63966e2339
azizjan.ayupov <azizjan.ayupov@gmail.com>
2024-07-18 10:34:55 +0600

    dockerization. not working

--- Webhook.Controllers/Program.cs
+++ Webhook.Controllers/Program.cs
@@ -5,9 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Setup bot configuration
 var botConfigSection = builder.Configuration.GetSection("BotConfiguration");
+
 builder.Services.Configure<BotConfiguration>(botConfigSection);
 builder.Services.AddHttpClient("tgwebhook").RemoveAllLoggers().AddTypedClient<ITelegramBotClient>(
     httpClient => new TelegramBotClient(botConfigSection.Get<BotConfiguration>()!.BotToken, httpClient));
+
 builder.Services.AddSingleton<Webhook.Controllers.Services.UpdateHandler>();
 builder.Services.ConfigureTelegramBotMvc();
 
